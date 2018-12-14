78117d57d64101c5198c089f1be4082b216f64a1
AsterLineS <43178977+AsterLineS@users.noreply.github.com>
2018-12-14 11:10:34 +0800

    ChineseCalendar

---
+++ NBACalendar/main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChineseCalendar
+{
+    class main
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+        }
+    }
+}
